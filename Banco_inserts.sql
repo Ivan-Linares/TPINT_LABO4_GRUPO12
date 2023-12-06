02b933715052ba2de9578fbf6853856010f96a6b
Hfroiman <hernan.froiman@alumnos.frgp.utn.edu.ar>
2023-12-06 17:01:06 -0300

    inserts

--- Banco_inserts.sql
+++ Banco_inserts.sql
@@ -1,10 +1,11 @@
 use Banco;
 
-INSERT INTO Tipos_Usuarios (Descripcion) VALUES ("Administrador");
-insert into Tipos_Usuarios (Descripcion)  values("Cliente");
+select *  from Usuarios
+INSERT INTO Tipos_Usuarios  VALUES ("1","Administrador");
+insert into Tipos_Usuarios  values("2","Cliente");
 
 INSERT INTO Usuarios (Usuario, Password, Tipo_Usuario, Estado) VALUES
-('admin1', 'pass1', 1, 1),
+('adm', 'pas', 1, 1),
 ('admin2', 'pass2', 1, 1),
 ('user1', 'pass1', 2, 1),
 ('user2', 'pass2', 2, 1),
@@ -57,35 +58,31 @@ INSERT INTO Localidades (codPais, codProvincia, NombreLocalidad) VALUES
 (3, 3, 'Feira de Santana'),
 (3, 3, 'Vitória da Conquista');
 
-INSERT INTO clientes (DNI, Cuil, Apellido, Nombre, Sexo, FechaNac, Direccion, Localidad, Email, Usuario, Estado)
+INSERT INTO clientes (DNI, Cuil, Apellido, Nombre, Sexo, FechaNac, Direccion, Localidad, Email, Telefono, Usuario, Estado)
 VALUES
 
-('12345678', '20345678901', 'Gomez', 'Juan', 'M', '1990-01-15', 'Calle 123', 1, 'juan@gmail.com', 'user1', 'P'),
-('23456789', '30345678901', 'Lopez', 'Ana', 'F', '1995-05-20', 'Calle 456', 3, 'ana@gmail.com', 'user2', 'P'),
-('34567890', '40345678901', 'Martinez', 'Pedro', 'M', '1988-08-10', 'Calle 789', 3, 'pedro@gmail.com', 'user3', 'P'),
-('45678901', '50345678901', 'Fernandez', 'Luis', 'M', '1992-07-08', 'Avenida A', 1, 'luis@gmail.com', 'user4', 'P');
+('12345678', '20345678901', 'Gomez', 'Juan', 'M', '1990-01-15', 'Calle 123', 1, 'juan@gmail.com',"1155885588", 'user1', 'A'),
+('23456789', '30345678901', 'Lopez', 'Ana', 'F', '1995-05-20', 'Calle 456', 3, 'ana@gmail.com',"1146468899" , 'user2', 'A'),
+('34567890', '40345678901', 'Martinez', 'Pedro', 'M', '1988-08-10', 'Calle 789', 3, 'pedro@gmail.com',"1188889999" , 'user3', 'A'),
+('45678901', '50345678901', 'Fernandez', 'Luis', 'M', '1992-07-08', 'Avenida A', 1, 'luis@gmail.com',"1138696933" ,'user4', 'P');
 
-
-INSERT INTO Telefonos (DNI, Telefono) VALUES
-('12345678', '1111111111'),
-('23456789', '2222222222'),
-('34567890', '3333333333'),
-('45678901', '4444444444');
 
 INSERT INTO Tipos_Cuentas (Descripcion) VALUES
 ('Caja de ahorro'),
 ('Cuenta Corriente');
 
+Select * from Cuentas;
+
 INSERT INTO Cuentas (DNI, Fecha_creacion, Tipo_De_Cuenta, CBU, Saldo, Estado) VALUES
-('12345678', '2023-11-18', 1, 'CBU111111111', 1500.00, 'A'),
-('23456789', '2023-11-18', 1, 'CBU222222222', 2000.00, 'A'),
-('34567890', '2023-11-18', 1, 'CBU333333333', 3000.00, 'A'),
-('45678901', '2023-11-18', 2, 'CBU444444444', 5000.00, 'A'),
-('12345678', '2023-11-18', 2, 'CBU555555555', 8000.00, 'A'),
-('34567890', '2023-11-18', 2, 'CBU666666666', 10000.00, 'A'),
-('45678901', '2023-11-18', 1, 'CBU777777777', 1200.00, 'A'),
-('45678901', '2023-11-18', 1, 'CBU888888888', 1800.00, 'A'),
-('34567890', '2023-11-18', 1, 'CBU999999999', 2500.00, 'A');
+('23456789', '2023-11-18', 1, '222222222', "2000.00", 'A'),
+('34567890', '2023-11-18', 1, '333333333', "3000.00", 'A'),
+('45678901', '2023-11-18', 2, '444444444', "5000.00", 'A'),
+('12345678', '2023-11-18', 2, '555555555', "8000.00", 'A'),
+('34567890', '2023-11-18', 2, '666666666', "10000.00", 'A'),
+('45678901', '2023-11-18', 1, '777777777', "1200.00", 'A'),
+('45678901', '2023-11-18', 1, '888888888', "1800.00", 'A'),
+('34567890', '2023-11-18', 1, '999999999', "2500.00", 'A'),
+('12345678', '2023-11-18', 1, '111111111', "1500.00", 'A');
 
 INSERT INTO Prestamos (Cuenta, dni, Fecha, Importe_mensual_a_pagar, Importe_total, Estado, cuotas_pendientes) VALUES
 (100000, '12345678', '2023-11-18', 200.00, 1200.00, 'P', '12'),
@@ -100,14 +97,17 @@ INSERT INTO Tipos_Movimientos (Descripcion) VALUES
 ('TransferenciaExtraccion'),
 ('TransferenciaDeposito');
 
+select * from Clientes c inner join Usuarios u on c.Usuario = u.Usuario inner join Localidades l on c.Localidad = l.codLocalidad inner join Provincia p on l.codProvincia = p.codProvincia inner join Pais pa on p.codPais = pa.codPais
+
+
 INSERT INTO Movimientos (Cuenta, Fecha, Concepto, IMPORTE, Tipo_movimiento) VALUES
 (100000, '2023-11-18', 'Alta de cuenta', 1500.00, '1'),
 (100001, '2023-11-18', 'Alta de cuenta', 2000.00, '1'),
 (100002, '2023-11-18', 'Alta de cuenta', 3000.00, '1'),
 (100003, '2023-11-18', 'Alta de cuenta', 5000.00, '1'),
-
 (100000, '2023-11-18', 'Alta de prestamo', 200.00, '2'),
-(100001, '2023-11-18', 'Alta de prestamo', 150.00, '2'),
+(100001, '2023-11-18', 'Alta de prestamo', 150.00, '2');
+
 (100002, '2023-11-18', 'Alta de prestamo', 300.00, '2'),
 (100003, '2023-11-18', 'Alta de prestamo', 500.00, '2'),
 
@@ -115,4 +115,3 @@ INSERT INTO Movimientos (Cuenta, Fecha, Concepto, IMPORTE, Tipo_movimiento) VALU
 (100001, '2023-11-18', 'Pago de prestamo', 150.00, '3'),
 (100002, '2023-11-18', 'Pago de prestamo', 300.00, '3'),
 (100003, '2023-11-18', 'Pago de prestamo', 500.00, '3');
-
